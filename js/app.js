bebcf50480a42c7667f89ca7d459f9c94df1ccae
EnzoBnl <enzobonnal@gmail.com>
2020-10-15 16:58:48 +0200

    add shop design

--- js/app.js
+++ js/app.js
@@ -6,16 +6,16 @@ class App extends React.Component {
     }
     render() {
         console.log(this.state);
-        return <div>
+        return <div style={{overflowX: "auto"}}>
             <h1><a href="index.html" class="ancient">Adeptus Optimus</a></h1>
             <p class="ancientSub">"Support wiser choices, on behalf of the Emperor."</p>
             <br/>
             <WeaponsParamTable setAppState={this.setAppState}/>
-            <div class="w3-bar datasheetbg"><div class="w3-bar-item"></div></div>
+            <div class="w3-bar shop-bg"><div class="w3-bar-item"></div></div>
             <ProgressLog state={this.state.state} msg={this.state.msg}/>
             <div id="chart-title" class="chart-title"></div>
-            <div id="chart"></div>
-            <div class="w3-bar datasheetbg"><div class="w3-bar-item"></div></div>
+            <div id="chart" class="chart"></div>
+            <div class="w3-bar shop-bg"><div class="w3-bar-item"></div></div>
         </div>
     }
     setAppState(props) {
@@ -27,10 +27,11 @@ class ProgressLog extends React.Component {
     render() {
         if (this.props.state == "processing") {
             return <div>
-                        <div class="w3-animate-fading-fast">
-                            <img src="static/skull.png" width="50px"></img>
-                        </div>
-                        <span class="datasheet"> {this.props.msg}</span>
+                        <img class="w3-animate-fading-fast" src="static/skull.png" width="32px"></img>
+                        <span class="shop"> {this.props.msg} </span>
+                        <img class="w3-animate-fading-fast" src="static/skull.png" width="32px"></img>
+
+                        <p><img src="static/testing.gif" width="256px"></img></p>
                    </div>
         } else if (this.props.state == "error") {
             return <div>
@@ -57,7 +58,7 @@ class WeaponsParamTable extends React.Component {
         SB: "2D6",
         WSBSA: "3",
         WSBSB: "5",
-        nameA: 'Bolt Rifle(15") on Intercessor',
+        nameA: 'Bolt Rifle (15") on Intercessor',
         nameB: "SAG on Big Mek",
         pointsA: "20",
         pointsB: "120",
@@ -70,14 +71,13 @@ class WeaponsParamTable extends React.Component {
     event.preventDefault();
     document.getElementById('chart').innerHTML = "";
     document.getElementById('chart-title').innerHTML = "";
-    this.setAppState({state: "processing", msg: "Firing on some captives Grots..."});
+    this.setAppState({state: "processing", msg: "Testing weapons..."/*"Firing on some captives Grots..."*/});
 
     // create a new XMLHttpRequest
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
     // get a callback when the server responds
     xhr.onload = () => {
-      // update the state of the component with the result here
       console.log("console.log(xhr.responseText):");
       console.log(xhr.response);
       if (xhr.status == 200) {
@@ -89,13 +89,13 @@ class WeaponsParamTable extends React.Component {
         });
       }
     };
+    // get a callback when net::ERR_CONNECTION_REFUSED
     xhr.onerror = () => {
-        // update the state of the component with the result here
         console.log("console.log(xhr.responseText):");
         console.log(xhr.response);
         this.setAppState({
             state: "error",
-            msg: "SERVER DOWN: The Forge World of Adeptus Optimus must be facing an onslaught of heretics."
+            msg: "SERVER DOWN: The Forge World of the Adeptus Optimus must be facing an onslaught of heretics."
         });
     };
     // open the request with the verb and the url
@@ -116,21 +116,21 @@ class WeaponsParamTable extends React.Component {
   render() {
     return (
       <form onSubmit={this.handleSubmit}>
-      <table class="w3-table w3-bordered datasheet">
-          <tr style={{background: "#b4bbb4", color: "black"}}>
+      <table class="w3-table w3-bordered">
+          <tr class="shop-bg">
             <th></th>
-            <th>NAME</th>
-            <th>WS/BS</th>
-            <th>A</th>
-            <th>S</th>
-            <th>AP</th>
-            <th>D</th>
-            <th>POINTS</th>
+            <th class="datasheet-header">NAME</th>
+            <th class="datasheet-header">WS/BS</th>
+            <th class="datasheet-header">A</th>
+            <th class="datasheet-header">S</th>
+            <th class="datasheet-header">AP</th>
+            <th class="datasheet-header">D</th>
+            <th class="datasheet-header">POINTS</th>
           </tr>
           <tr>
-            <th style={{background: "#7dae3e", color: "white"}}>Weapon A</th>
+            <th  class="datasheet-header" style={{background: "#6d9e2e", color: "white"}}>Weapon A</th>
             <th class="datasheet">
-                <input
+                <input maxlength="32"
                     id="nameA"
                     type="text"
                     style={{width: "125px"}}
@@ -138,33 +138,33 @@ class WeaponsParamTable extends React.Component {
                     onChange={this.handleWeaponParamsChange}
                     ></input>
             </th>
-            <th><input id="WSBSA" value={this.state.WSBSA} type="text" class="datasheet input-dice-right" onChange={this.handleWeaponParamsChange}></input>+</th>
-            <th><input id="AA" value={this.state.AA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th><input id="SA" value={this.state.SA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th>-<input id="APA" value={this.state.APA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th><input id="DA" value={this.state.DA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th><input id="pointsA" value={this.state.pointsA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="WSBSA" value={this.state.WSBSA} type="text" class="datasheet input-dice-right" onChange={this.handleWeaponParamsChange}></input>+</th>
+            <th><input maxlength="4" id="AA" value={this.state.AA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="SA" value={this.state.SA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th>-<input maxlength="4" id="APA" value={this.state.APA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="DA" value={this.state.DA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="pointsA" value={this.state.pointsA} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
           </tr>
           <tr>
-            <th style={{background: "#0d407f", color: "white"}}>Weapon B</th>
+            <th class="datasheet-header" style={{background: "#0d407f", color: "white"}}>Weapon B</th>
             <th class="datasheet">
-                <input
+                <input maxlength="32"
                     id="nameB"
                     type="text"
                     style={{width: "125px"}}
                     value={this.state.nameB}
                     onChange={this.handleWeaponParamsChange}></input>
             </th>
-            <th><input id="WSBSB" value={this.state.WSBSB} type="text" class="datasheet input-dice-right" onChange={this.handleWeaponParamsChange}></input>+</th>
-            <th><input id="AB" value={this.state.AB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th><input id="SB" value={this.state.SB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th>-<input id="APB" value={this.state.APB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th><input id="DB" value={this.state.DB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
-            <th><input id="pointsB" value={this.state.pointsB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="WSBSB" value={this.state.WSBSB} type="text" class="datasheet input-dice-right" onChange={this.handleWeaponParamsChange}></input>+</th>
+            <th><input maxlength="4" id="AB" value={this.state.AB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="SB" value={this.state.SB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th>-<input maxlength="4" id="APB" value={this.state.APB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="DB" value={this.state.DB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
+            <th><input maxlength="4" id="pointsB" value={this.state.pointsB} type="text" class="datasheet input-dice-left" onChange={this.handleWeaponParamsChange}></input></th>
           </tr>
       </table>
       <br/>
-      <input type="submit" value="Compare" class="w3-btn datasheet" style={{background: "#b4bbb4", color: "black"}}/>
+      <input maxlength="4" type="submit" value="Compare" class="w3-btn shop shop-bg"/>
       </form>
     );
   }
