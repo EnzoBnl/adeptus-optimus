202f4c9700068171d036584c2e4b56fa01d4ce9c
EnzoBnl <enzobonnal@gmail.com>
2020-10-31 15:51:35 +0100

    ParamsTable

--- js/app.js
+++ js/app.js
@@ -119,12 +119,16 @@ class App extends React.Component {
         console.log("App render() called")
         console.log(this.state);
         return <div>
+            <Login initState={{id: this.state.id, token: this.state.token}} sendCredentialsToApp={this.sendCredentialsToApp}/>
             <h1><a href="index.html" className="title">Adeptus Optimus</a></h1>
             <p className="title subscript">" Support wiser choices, on behalf of the Emperor."</p>
-            <Login initState={{id: this.state.id, token: this.state.token}} sendCredentialsToApp={this.sendCredentialsToApp}/>
             <br/>
             <div style={{overflowX: "auto"}}>
-                <ParamsTables initParams={this.initParams} sendParamChange={this.sendParamChange}/>
+                <ParamsTable initParams={this.initParams} sendParamChange={this.sendParamChange} letter="A"/>
+                <br/>
+                <br/>
+                <br/>
+                <ParamsTable initParams={this.initParams} sendParamChange={this.sendParamChange} letter="B"/>
             </div>
             <br/>
             <button className="w3-btn greeny-bg datasheet-header" onClick={this.handleSubmit}>COMPARE</button>
@@ -219,13 +223,15 @@ class Help extends React.Component {
              return <div>
                         {this.button}
                         <div className="w3-content">
-                            <div className="w3-row-padding w3-center w3-margin-top shop">
+                            <div className="w3-row-padding w3-center w3-margin-top w3-margin-bottom shop">
                                 <InfoBox title="Adeptus Optimus" body={<p>The <i>Adeptus Optimus</i> is an analytics organization attached to the <i>Adeptus Mechanicus</i>. The <i>Adeptus Optimus Engine</i> has been built by an <i>Archimagus computus mathematica</i> to give to lords of war an <b>intuitive and rigorous tool</b> to guide their weapons choices.</p>}/>
-                                <InfoBox title="Attacking profiles" body={<p>The engine performs a comparison between two attacking profiles. Each <b>profile represents one or more models and their weapons</b>, with a cost associated with the whole. Each different weapon used by the attacking profile has to be declared along with a total number of <i>Shots</i> made with it during one phase, by the models of the profile.</p>}/>
+                                <InfoBox title="Attacking profiles" body={<p>The engine performs a comparison between two attacking profiles. Each <b>profile represents one or more models and their weapons</b>, with a cost associated with the whole. Each different weapon used by the attacking profile has to be declared along with a total number of <i>Attacks</i> made with it during one phase, by the models of the profile.</p>}/>
                                 <InfoBox title="Results" body={<p>The engine computes a precise <i>average number of target unit's models killed per profile point</i> for profiles A and B, against a <b>large variety of target units defense profiles</b>. The engine leverages advanced algorithmic to compute deterministic calculus leading to almost exact results.</p>}/>
                                 <InfoBox title="The ultimate engine" body={<p>The entire dice rolls sequences are theoretically modeled, making the <i>Adeptus Optimus Engine</i> the only tool handling <b>complex effects of random damages characteristics and <i>Feel No Pains</i></b> during the sequential damage allocation step, or the <b>threshold effects introduced by a random <i>Strength</i> characteristic</b>, among others.</p>}/>
                             </div>
                         </div>
+                        <div className="w3-bar greeny-bg"><div className="w3-bar-item"></div></div>
+
                     </div>
         } else {
             return <span>{this.button}</span>
@@ -254,26 +260,28 @@ class ProgressLog extends React.Component {
     }
 }
 
-class ParamsTables extends React.Component {
+
+class ParamsTable extends React.Component {
     constructor(props) {
         super(props);
+        this.weaponsVisibility = [true, false];
     }
 
     render() {
-     return <div>
-                <table className="w3-table w3-bordered nowrap">
-                    <ProfileTable bg="profile-a-bg" letter="A" initParams={this.props.initParams} sendParamChange={this.props.sendParamChange}/>
-                    <WeaponTable id="A.1"/>
-                    <WeaponTable id="A.2"/>
-                </table>
-                <br/>
-                <br/>
-                <br/>
-                <table className="w3-table w3-bordered nowrap">
-                    <ProfileTable bg="profile-b-bg" letter="B" initParams={this.props.initParams} sendParamChange={this.props.sendParamChange}/>
-                    <WeaponTable id="B.1"/>
-                </table>
-            </div>;
+
+        if (this.weaponsVisibility.includes(false)) {
+            var buttonStyle = {};
+        } else {
+            var buttonStyle = {"display": "hidden"};
+        }
+        return <table className="w3-table w3-bordered nowrap">
+                <ProfileTable bg={"profile-" + this.props.letter + "-bg"} letter={this.props.letter} initParams={this.props.initParams} sendParamChange={this.props.sendParamChange}/>
+                <WeaponTable visible={this.weaponsVisibility[0]}  id={this.props.letter + ".1"} initParams={this.props.initParams} sendParamChange={this.props.sendParamChange}/>
+                <WeaponTable visible={this.weaponsVisibility[1]}  id={this.props.letter + ".2"} initParams={this.props.initParams} sendParamChange={this.props.sendParamChange}/>
+                <tbody><tr>
+                    <th><button className="logo-btn"><i className="fa"><b>+</b></i></button></th>
+                </tr></tbody>
+            </table>;
     }
 }
 
@@ -295,15 +303,17 @@ class ProfileTable extends React.Component {
     render() {
         console.log("profile" + this.props.letter + " state", this.state);
         return  <tbody>
-                  <tr className="shop-bg datasheet-header">
-                    <th style={{background: "#fff"}}></th>
-                    <th>NAME</th>
-                    <th>POINTS</th>
-                  </tr>
                   <tr className="datasheet-body">
-                      <th className={"datasheet-header profile-flag " + this.props.bg}>Profile {this.props.letter}</th>
-                      <th><input maxLength="32" id="name" type="text" className="input input-name" value={this.state.name} onChange={this.handleChange} ></input></th>
-                      <th><input maxLength="4" id="points" value={this.state.points} type="text" className="input input-dice align-right" onChange={this.handleChange}></input></th>
+                    <th className={"datasheet-header profile-flag " + this.props.bg}>Profile {this.props.letter}</th>
+                    <th><input maxLength="4" id="points" value={this.state.points} type="text" className="input input-dice align-right" onChange={this.handleChange}></input> points</th>
+                  </tr>
+                  <tr className="datasheet-header">
+                    <th><input maxLength="32" id="name" type="text" className="white-bg datasheet-body input input-profile-name" value={this.state.name} onChange={this.handleChange} ></input></th>
+                    <th className="greeny-bg">Attacks</th>
+                    <th className="greeny-bg">WS/BS</th>
+                    <th className="greeny-bg">S</th>
+                    <th className="greeny-bg">AP</th>
+                    <th className="greeny-bg">D</th>
                   </tr>
                 </tbody>;
     }
@@ -311,23 +321,26 @@ class ProfileTable extends React.Component {
 
 class WeaponTable extends React.Component {
     constructor(props) {
-        super(props)
+        super(props);
+        this.state = {}
+        this.state.name = this.props.initParams["name" + this.props.id];
+    }
+
+    handleChange(event) {
+        this.state[event.target.id] = event.target.value;
+        this.props.sendParamChange(event.target.id + this.props.id, event.target.value);
     }
 
     render() {
-        return <tbody>
-                  <tr className="greeny-bg datasheet-header">
-                    <th style={{background: "#fff"}}></th>
-                    <th>Shots</th>
-                    <th>WS/BS</th>
-                    <th>S</th>
-                    <th>AP</th>
-                    <th>D</th>
-                  </tr>
-                  <tr className="datasheet-body">
-                    <th  className="datasheet-header weapon-flag">Weapon {this.props.id}</th>
-                  </tr>
-               </tbody>;
+        if(this.props.visible) {
+            return <tbody>
+                      <tr className="datasheet-body">
+                        <th><button className="logo-btn"><i className="fa fa-trash"></i></button> <input maxLength="32" id="name" type="text" className="white-bg datasheet-body input input-weapon-name" value={this.state.name} onChange={this.handleChange} ></input></th>
+                      </tr>
+                   </tbody>;
+        } else {
+            return <tbody></tbody>
+        }
     }
 }
 
